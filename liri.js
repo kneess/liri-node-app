c0dc899b66b6c96870d1130a6d2272a5afbc8e09
knees <you@example.com>
2018-04-08 16:14:33 -0700

    getting a response from spotify

--- liri.js
+++ liri.js
@@ -60,16 +60,17 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   } 
 
   function spotify() {
+    
     var Spotify = require('node-spotify-api');
  
+    var spotify = new Spotify(keys.spotify);
  
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+    spotify.search({ type: 'track', query: 'milkshake', limit: 1 })
+    .then(function(response) {
+      console.log(response);
+    }).catch(function(err) {
+      console.log(err);
+    });
   }
 
 
