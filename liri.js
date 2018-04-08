f85d9c5dc055b7424ea9169d53b6ec03aef047ab
knees <you@example.com>
2018-04-08 15:57:20 -0700

    tweets complete

--- liri.js
+++ liri.js
@@ -1,22 +1,27 @@
 require("dotenv").config();
 
-
+//importing information from keys file
  var keys = require("./keys");
-
- var spotifyid = keys.spotify.id;
- var spotifysecret = keys.spotify.secret;
-console.log(spotifysecret);
+//spotify keys 
+//  var spotifyid = keys.spotify.id;
+//  var spotifysecret = keys.spotify.secret;
+//  var Tkey = keys.twitter.consumer_key;
+//  var Tsecret = keys.twitter.consumer_secret;
+//  var Ttoken = keys.twitter.access_token_key;
+//  var Ttokensecret = keys.twitter.access_token_secret;
+//  var spotify = new Spotify(keys.spotify);
 //  var client = new Twitter(keys.twitter);
-
+//  console.log(spotify);
+//taking input from the console
 var command = process.argv[2];
 
 var args = process.argv;
-
+//save the user input
  var userInput ='';
-
+//for loop to take all words in after the command
  for (var i = 3; i < args.length; i++) {
 
-  // Build a string with the address.
+  // Build a string with the user input
   userInput = userInput + " " + args[i];  
 }
 
@@ -24,11 +29,11 @@ var args = process.argv;
   case "my-tweets":
   tweets();
     break;
+ 
+  case "spotify-this-song":
+    spotify();
+    break;
  }
-//   case "spotify-this-song":
-//     spotify();
-//     break;
-  
 //   case "movie-this":
 //     movie();
 //     break;
@@ -37,9 +42,34 @@ var args = process.argv;
 //     dowhat();
 //     break;
 //   }
+   
+  function tweets() {
+  
+  var Twitter = require('twitter');
+  var client = new Twitter(keys.twitter);
+  var params = {screen_name: '@Anibalicz', count: 20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    for (var i = 0; i < tweets.length; i++) {
+      console.log("Tweets: " +tweets[i].text + "\nCreated at: " +tweets[i].created_at);
+    }
+    // console.log(tweets[0].text);
+  }
+});
 
-  // function tweets() {
-    
-  // } 
+  } 
+
+  function spotify() {
+    var Spotify = require('node-spotify-api');
+ 
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+  }
 
 
