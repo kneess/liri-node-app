f03f170079cf7da24417fedeb63d01df2fdc2eb5
knees <you@example.com>
2018-04-08 17:14:56 -0700

    retrieving information needed for spotify app

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 //importing information from keys file
- var keys = require("./keys");
+var keys = require("./keys");
 //spotify keys 
 //  var spotifyid = keys.spotify.id;
 //  var spotifysecret = keys.spotify.secret;
@@ -17,60 +17,64 @@ var command = process.argv[2];
 
 var args = process.argv;
 //save the user input
- var userInput ='';
+var userInput = '';
 //for loop to take all words in after the command
- for (var i = 3; i < args.length; i++) {
+for (var i = 3; i < args.length; i++) {
 
   // Build a string with the user input
-  userInput = userInput + " " + args[i];  
+  userInput = userInput + " " + args[i];
 }
 
- switch (command) {
+switch (command) {
   case "my-tweets":
-  tweets();
+    tweets();
     break;
- 
+
   case "spotify-this-song":
     spotify();
     break;
- }
+}
 //   case "movie-this":
 //     movie();
 //     break;
-  
+
 //   case "do-what-it-says":
 //     dowhat();
 //     break;
 //   }
-   
-  function tweets() {
-  
+
+function tweets() {
+
   var Twitter = require('twitter');
+
   var client = new Twitter(keys.twitter);
-  var params = {screen_name: '@Anibalicz', count: 20};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for (var i = 0; i < tweets.length; i++) {
-      console.log("Tweets: " +tweets[i].text + "\nCreated at: " +tweets[i].created_at);
+
+  var params = { screen_name: '@Anibalicz', count: 20 };
+  client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log("Tweets: " + tweets[i].text + "\nCreated at: " + tweets[i].created_at);
+      }
+      // console.log(tweets[0].text);
     }
-    // console.log(tweets[0].text);
-  }
-});
+  });
 
-  } 
+}
 
-  function spotify() {
-    
-    var Spotify = require('node-spotify-api');
- 
-    var spotify = new Spotify(keys.spotify);
- 
-    spotify.search({ type: 'track', query: 'milkshake', limit: 1 })
-    .then(function(response) {
-      console.log(response);
-    }).catch(function(err) {
-      console.log(err);
-    });
-  }
+function spotify() {
+
+  var Spotify = require('node-spotify-api');
+
+  var spotify = new Spotify(keys.spotify);
+
+  spotify.search({ type: 'track', query: 'milkshake', limit: 1 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    console.log('Artists: ' + data.tracks.items[0].album.artists[0].name + '\nSong: ' + data.tracks.items[0].name +
+      '\nPreview: ' + data.tracks.items[0].preview_url + '\nAlbum: ' + data.tracks.items[0].album.name);
+  });
+}
 
 
